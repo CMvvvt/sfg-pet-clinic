7eb9a0f32f279c37b2f066594c04bb66919843c2
CMvvvt <452000761@qq.com>
2021-08-11 22:48:12 +0800

    added PetTypeService. closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author cmchenming13@gmail.com
+ * @date 2021/8/11 22:43
+ */
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+
+}
